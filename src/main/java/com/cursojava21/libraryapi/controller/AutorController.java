da75a6b008c5a710ddb05c0ee9259d3717fc177f
André Machado <andrels.machado82@gmail.com>
2026-02-21 17:54:02 -0300

    Criação da lógica para cadastrar um Autor

--- src/main/java/com/cursojava21/libraryapi/controller/AutorController.java
+++ src/main/java/com/cursojava21/libraryapi/controller/AutorController.java
@@ -1,4 +1,28 @@
 package com.cursojava21.libraryapi.controller;
 
+import com.cursojava21.libraryapi.dto.AutorRequestDTO;
+import com.cursojava21.libraryapi.dto.AutorResponseDTO;
+import com.cursojava21.libraryapi.services.AutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/autores")
+@RequiredArgsConstructor
 public class AutorController {
+
+    private final AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorResponseDTO> create(@Validated @RequestBody AutorRequestDTO authorRequestDTO) {
+        AutorResponseDTO response = autorService.create(authorRequestDTO);
+        return ResponseEntity.created(URI.create("/autores/" + response.getId())).body(response);
+    }
 }
--- src/main/java/com/cursojava21/libraryapi/services/AutorService.java
+++ src/main/java/com/cursojava21/libraryapi/services/AutorService.java
@@ -1,4 +1,25 @@
 package com.cursojava21.libraryapi.services;
 
+import com.cursojava21.libraryapi.repository.AutorRepository;
+import com.cursojava21.libraryapi.dto.AutorRequestDTO;
+import com.cursojava21.libraryapi.dto.AutorResponseDTO;
+import com.cursojava21.libraryapi.mapper.AutorMapper;
+import com.cursojava21.libraryapi.model.Autor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AutorService {
+
+    private final AutorRepository autorRepository;
+    private final AutorMapper mapper;
+
+    public AutorResponseDTO create(AutorRequestDTO autorRquestDTO) {
+        Autor autor = mapper.toEntity(autorRquestDTO);
+
+        Autor saved = autorRepository.save(autor);
+
+        return mapper.toResponseDTO(saved);
+    }
 }
